ecc6e9f5d28d8e365f18da62df4ebfbd4e95c721
Foysal Ahmed <foysal.ahmed@strativ.se>
2025-10-14 10:52:01 +0600

    feat: update translation

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,5 +1,6 @@
 import { WarningOutlined } from '@ant-design/icons';
 import { Flex } from 'antd';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 import { Text } from '@/components/Atoms';
@@ -7,6 +8,8 @@ import { Text } from '@/components/Atoms';
 import AntitledCoverPhoto from '@/assets/images/antitled-cover.png';
 
 export default function ComponentsPage() {
+  const { t } = useTranslation();
+
   return (
     <Container>
       <Flex vertical justify='center' gap={16}>
@@ -18,25 +21,14 @@ export default function ComponentsPage() {
         <Text size='display-md' weight='regular'>
           Note:
         </Text>
-        <Text size='text-lg'>
-          This project is to track the project of a component library for our
-          own. It contains the Untitled UI component set, which serves as our
-          design foundation in most of the projects.
-        </Text>
+        <Text size='text-lg'>{t('features.components.welcome_text_1')}</Text>
 
-        <Text size='text-lg'>
-          In frontend, Ant Design components will be customized to visually
-          match these Untitled UI components. The goal is to save time in future
-          projects by standardizing this process into a ready-to-use library.
-        </Text>
+        <Text size='text-lg'>{t('features.components.welcome_text_2')}</Text>
 
         <Flex gap={40} align='center'>
-          <WarningOutlined style={{ color: 'orange', fontSize: 72 }} />
+          <WarningOutlined style={{ color: '#F04438', fontSize: 72 }} />
           <Text size='text-lg' color='textErrorPrimary600'>
-            Disclaimer: This is a base collection with core components and
-            simple variants. Individual projects may extend it with new
-            components, additional variants or even a whole custom design
-            approach.
+            {t('features.components.disclaimer_text')}
           </Text>
         </Flex>
       </Flex>
--- src/lib/translation/locales/en.ts
+++ src/lib/translation/locales/en.ts
@@ -14,6 +14,14 @@ export const en = {
         please_input_your_password: 'Please input your password',
         logout: 'Logout',
       },
+      components: {
+        welcome_text_1:
+          'This project is to track the project of a component library for our own. It contains the Untitled UI component set, which serves as our design foundation in most of the projects.',
+        welcome_text_2:
+          'In frontend, Ant Design components will be customized to visually match these Untitled UI components. The goal is to save time in future projects by standardizing this process into a ready-to-use library.',
+        disclaimer_text:
+          'Disclaimer: This is a base collection with core components and simple variants. Individual projects may extend it with new components, additional variants or even a whole custom design approach.',
+      },
     },
     pages: {
       dashboard: {
