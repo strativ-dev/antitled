fd9edfeff49375365aabff1fc7cfb8c160f9a8e9
Foysal Ahmed <foysal.ahmed@strativ.se>
2026-01-09 08:21:37 +0600

    feat: add responsive prop for Text

--- src/components/Atoms/Text/index.tsx
+++ src/components/Atoms/Text/index.tsx
@@ -7,6 +7,7 @@ import {
 import styled from 'styled-components';
 
 import {
+  Breakpoint,
   LineHeightVariant,
   TextColorVariant,
   TextSizeVariant,
@@ -32,15 +33,24 @@ const parseVariant = (
   return { size, weight, lineHeight };
 };
 
+// Utility type to allow autocomplete for specific values while accepting any string
+type LooseAutocomplete<T extends string> = T | Omit<string, T>;
+
 type PolymorphicTextProps<C extends ElementType = 'span'> = {
   variant?: TextVariant;
-  color?: TextColorVariant | (string & {});
+  color?: LooseAutocomplete<TextColorVariant>;
   children: ReactNode;
   as?: C;
   className?: string;
   style?: CSSProperties;
   margin?: string;
   padding?: string;
+  // Responsive variants
+  smallMobile?: TextVariant;
+  mobile?: TextVariant;
+  tablet?: TextVariant;
+  laptop?: TextVariant;
+  desktop?: TextVariant;
 } & ComponentPropsWithoutRef<C>;
 
 export type TextProps = PolymorphicTextProps;
@@ -55,10 +65,24 @@ export const Text = <C extends ElementType = 'span'>({
   margin = '0px 0px',
   padding = '0px 0px',
   onClick,
+  smallMobile,
+  mobile,
+  tablet,
+  laptop,
+  desktop,
   ...props
 }: PolymorphicTextProps<C>) => {
   const Component = as || 'span';
   const { size, weight, lineHeight } = parseVariant(variant);
+
+  // Parse responsive variants
+  const smallMobileVariant = smallMobile
+    ? parseVariant(smallMobile)
+    : undefined;
+  const mobileVariant = mobile ? parseVariant(mobile) : undefined;
+  const tabletVariant = tablet ? parseVariant(tablet) : undefined;
+  const laptopVariant = laptop ? parseVariant(laptop) : undefined;
+  const desktopVariant = desktop ? parseVariant(desktop) : undefined;
 
   return (
     <Wrapper
@@ -72,19 +96,35 @@ export const Text = <C extends ElementType = 'span'>({
       onClick={onClick}
       $margin={margin}
       $padding={padding}
+      $smallMobile={smallMobileVariant}
+      $mobile={mobileVariant}
+      $tablet={tabletVariant}
+      $laptop={laptopVariant}
+      $desktop={desktopVariant}
       {...props}>
       {children}
     </Wrapper>
   );
 };
 
+type ResponsiveVariant = {
+  size: TextSizeVariant;
+  weight: WeightVariant;
+  lineHeight: LineHeightVariant;
+};
+
 type WrapperProps = {
   $size: TextSizeVariant;
   $weight: WeightVariant;
   $lineHeight: LineHeightVariant;
-  $color: string;
+  $color: LooseAutocomplete<TextColorVariant>;
   $margin?: string;
   $padding?: string;
+  $smallMobile?: ResponsiveVariant;
+  $mobile?: ResponsiveVariant;
+  $tablet?: ResponsiveVariant;
+  $laptop?: ResponsiveVariant;
+  $desktop?: ResponsiveVariant;
 };
 
 const Wrapper = styled.span<WrapperProps>`
@@ -95,6 +135,55 @@ const Wrapper = styled.span<WrapperProps>`
     theme.colors.texts[$color as TextColorVariant] ?? $color};
   margin: ${({ $margin }) => $margin};
   padding: ${({ $padding }) => $padding};
-  display: inline-block;
-  line-height: ${({ $size, theme }) => theme.lineHeight[$size]};
+  /* display: inline-block; */
+
+  ${({ $smallMobile, theme }) =>
+    $smallMobile &&
+    `
+    ${Breakpoint.smallMobile} {
+      font-size: ${theme.fontSize[$smallMobile.size]};
+      font-weight: ${theme.fontWeight[$smallMobile.weight]};
+      line-height: ${theme.lineHeight[$smallMobile.lineHeight]};
+    }
+  `}
+
+  ${({ $mobile, theme }) =>
+    $mobile &&
+    `
+    ${Breakpoint.mobile} {
+      font-size: ${theme.fontSize[$mobile.size]};
+      font-weight: ${theme.fontWeight[$mobile.weight]};
+      line-height: ${theme.lineHeight[$mobile.lineHeight]};
+    }
+  `}
+
+  ${({ $tablet, theme }) =>
+    $tablet &&
+    `
+    ${Breakpoint.tablet} {
+      font-size: ${theme.fontSize[$tablet.size]};
+      font-weight: ${theme.fontWeight[$tablet.weight]};
+      line-height: ${theme.lineHeight[$tablet.lineHeight]};
+    }
+  `}
+
+  ${({ $laptop, theme }) =>
+    $laptop &&
+    `
+    ${Breakpoint.laptop} {
+      font-size: ${theme.fontSize[$laptop.size]};
+      font-weight: ${theme.fontWeight[$laptop.weight]};
+      line-height: ${theme.lineHeight[$laptop.lineHeight]};
+    }
+  `}
+
+  ${({ $desktop, theme }) =>
+    $desktop &&
+    `
+    ${Breakpoint.desktop} {
+      font-size: ${theme.fontSize[$desktop.size]};
+      font-weight: ${theme.fontWeight[$desktop.weight]};
+      line-height: ${theme.lineHeight[$desktop.lineHeight]};
+    }
+  `}
 `;
